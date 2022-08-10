8da2a78234be4f14aaf5dcfa223e1b059c15f65a
Leia Renée <leia.renee@devsecops-academy.com>
2022-08-10 13:14:36 +0300

    add job resource output

--- test/main.tf
+++ test/main.tf
@@ -5,3 +5,9 @@ resource "random_pet" "this" {
 output "random_pet" {
   value = random_pet.this.id
 }
+
+output "job_resources" {
+  value = {
+    "Apache_Machine" : "[LINK]https://example.com/apache/sample.php[/LINK]"
+  }
+}
